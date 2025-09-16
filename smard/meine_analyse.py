de955c7656878d32ec1064095b1de419c27810c1
Eduard Moser <eduard.moser@gmx.de>
2025-09-16 17:12:24 +0200

    senec analyse

--- smard/meine_analyse.py
+++ smard/meine_analyse.py
@@ -12,15 +12,148 @@ logger = logging.getLogger(__name__)
 
 DEBUG = False
 
-class MeineAnalyse:
+
+        
+class Analyse(object):
+
+    def __init__(self, data):
+        # self.region = None
+        self.data = data
+        # self.battery_results = None
+        # pass
+
+    def simulate_battery(self,capacity=20000, power=10000):        
+        container = []
+        residual = []
+        exflow = []
+        battery_exflow= []
+        battery_inflow= []
+        exflow = []
+        fillstand = capacity / 2
+        for w,d in zip(self.data["my_renew"],self.data["my_demand"]):
+            p = w-d
+            diff = max(-power,min(power,p))
+            if p > 0:
+                battery_inflow.append(diff)
+                battery_exflow.append(0)
+                fillstand = min(capacity, fillstand+diff)
+                if fillstand+diff > capacity:
+                    exflow.append(fillstand+diff-capacity)
+                else:
+                    exflow.append(0)                    
+                residual.append(0)
+            else:
+                battery_inflow.append(0)
+                if fillstand <= 0:
+                    battery_exflow.append(0)
+                    fillstand = 0
+                    residual.append(fillstand+diff)
+                else:
+                    battery_exflow.append(diff)
+                    fillstand = fillstand+diff
+                    residual.append(0)
+                exflow.append(0)
+            container.append(fillstand)
+        self.data["battery"] = container
+        self.data["residual"] = residual
+        self.data["exflow"] = exflow
+        self.data["battery_inflow"] = battery_inflow
+        self.data["battery_exflow"] = battery_exflow
+        share = (sum(self.data["my_demand"]) + sum(residual))/sum(self.data["my_demand"])
+        results = pd.DataFrame([[capacity/1000,f"{(-sum(residual)/1000):.2f}",f"{(sum(exflow)/1000):.2f}", f"{share:.2f}"]], columns=["capacity MWh","residual MWh","exflow MWh", "share"])
+        self.battery_results = pd.concat([self.battery_results, results], ignore_index=True)
+        pass
+
+    def prepare_data(self):
+        pos, neg, exflow = [], [], []
+        for w,d in zip(self.data['my_renew'],self.data['my_demand']):
+            if w > d:
+                pos.append(d)
+                neg.append(0)
+                exflow.append(w-d)
+            else:
+                pos.append(w)
+                neg.append(d-w)
+        self.pos = pos
+        self.neg = neg
+        self.exflow = exflow
+        share = sum(self.pos)/sum(self.data["my_demand"])
+        self.battery_results = pd.DataFrame([[0,f"{(sum(self.neg)/1000):.2f}",f"{(sum(self.exflow)/1000):.2f}",f"{share:.2f}"]], columns=["capacity MWh","residual MWh","exflow MWh", "share"])
+        self.my_total_demand = self.data["my_demand"].sum()
+
+    def run_analysis(self, capaciy_list=[5000, 10000, 20000], power_list=[2500,5000,10000]):
+        """Run the analysis"""
+        if self.data is None:
+            print("❌ No data loaded!")
+            return
+        
+        logger.info("Starting analysis...")
+
+        self.prepare_data()
+        self.print_results()
+        for capacity, power in zip(capaciy_list, power_list):
+            self.simulate_battery(capacity=capacity, power=power)
+            self.print_results_with_battery()
+        # self.simulate_battery(capacity=10000, power=5000)
+        # self.print_results_with_battery()
+        # self.simulate_battery(capacity=20000, power=10000)
+        # self.print_results_with_battery()
+        print(self.battery_results)
+        self.visualise()
+        pass
+
+
+    def print_results(self):
+        print(f"reference region: {self.region}, demand: {(sum(self.data["total_demand"])/1000):.2f} GWh, solar: {(sum(self.data['solar'])/1000):.2f} GWh, wind {(sum(self.data["wind_onshore"])/1000):.2f} GWh")
+        print(f"total demand: {(sum(self.data["my_demand"])/1e3):.2f} MWh " +
+              f"total Renewable_Source: {(sum(self.data["my_renew"])/1e3):.2f} MWh")
+        print(f"total renewalbes: {(sum(self.pos)/1000):.2f} MWh, residual: {(sum(self.neg)/1000):.2f} MWh")
+        print(f"share without battery {(sum(self.pos)/self.my_total_demand):.2f}")
+
+    def print_results_with_battery(self):
+        res = -sum(self.data["residual"])
+        print(f"total renewalbes: {(sum(self.pos)/1000):.2f} MWh, residual: {(res/1000):.2f} MWh, export: {(sum(self.data["exflow"])/1000):.2f} MWh")
+        print(f"share with battery: {((self.my_total_demand - res)/self.my_total_demand):.2f}")
+        pass
+
+    def visualise(self, start=0, end=None):
+        if end is None:
+            end = len(self.data)        
+        fig, [ax1, ax2, ax3, ax4] = plt.subplots(4, 1, sharex=True)
+        ax1.plot(self.data.index[start:end], self.data["my_renew"][start:end], color="green")
+        ax1.plot(self.data.index[start:end], self.data["my_demand"][start:end], color="red")
+        ax1.set_ylabel("[kWh]")
+        ax1.set_title(f"Renewable_Source and Demand ({self.region})")
+        ax1.legend(["Renewable_Source", "Demand"])
+        ax1.grid(True)
+        ax2.plot(self.data.index[start:end], np.maximum(0,self.data["my_renew"][start:end]-self.data["my_demand"])[start:end], color="green")
+        ax2.plot(self.data.index[start:end], np.minimum(0,self.data["my_renew"][start:end]-self.data["my_demand"][start:end]), color="red")
+        ax2.legend(["Renewable_Source-Demand", "Residual"])
+        ax2.set_title("Renewable_Source-Demand")
+        ax2.set_ylabel("[kWh]")
+        ax2.grid(True)
+        ax3.plot(self.data.index[start:end], self.data["battery"][start:end], color = "blue")
+        # ax3.legend(["battery"])
+        ax3.set_title("battery fillstand")
+        ax3.set_ylabel("[kWh]")
+        ax4.plot(self.data.index[start:end], self.data["residual"][start:end], color="red")
+        #ax4.legend(["-Demand"])
+        ax4.set_title("Residual")
+        ax4.set_xlabel("Date")
+        ax4.set_ylabel("[kWh]")
+        ax4.grid(True)
+        plt.show()
+
+        pass
+
+class MeineAnalyse(Analyse):
 
     def __init__(self, csv_file_path, region = ""):
         """Initialize with German SMARD data"""
         self.region = region
-        self.data = self.load_and_prepare_data(csv_file_path)
-        share = sum(self.pos)/sum(self.data["my_demand"])
-        self.battery_results = pd.DataFrame([[0,f"{(sum(self.neg)/1000):.2f}",f"{(sum(self.exflow)/1000):.2f}",f"{share:.2f}"]], columns=["capacity MWh","residual MWh","exflow MWh", "share"])
-        
+        data = self.load_and_prepare_data(csv_file_path)
+        super().__init__(data)
+
     def load_and_prepare_data(self, csv_file_path):
         """Load and prepare SMARD data"""
         print("Loading SMARD data for European grid analysis...")
@@ -82,25 +215,12 @@ class MeineAnalyse:
                 total_installed_wind = 208
 
             ### this an extimate (for the time being seems better ...)
-            
+
             total_installed_solar = max(df["solar"])
             total_installed_wind = max(df["wind_onshore"])
             df["my_demand"] = df["total_demand"] * my_total_demand / total_demand * self.resolution
             df["my_renew"] = df["wind_onshore"] * 5000 / total_installed_wind * self.resolution
             df["my_renew"] += df["solar"] * 5000 / total_installed_solar * self.resolution
-
-            pos, neg, exflow = [], [], []
-            for w,d in zip(df['my_renew'],df['my_demand']):
-                if w > d:
-                    pos.append(d)
-                    neg.append(0)
-                    exflow.append(w-d)
-                else:
-                    pos.append(w)
-                    neg.append(d-w)
-            self.pos = pos
-            self.neg = neg
-            self.exflow = exflow
 
             # print(my_total_demand, sum(df["my_demand"]), sum(pos)+sum(neg))
             df = df.fillna(0)
@@ -117,99 +237,6 @@ class MeineAnalyse:
         else: #ion as e:
             print(f"Error loading data: {e}")
             return None
-
-    def simulate_battery(self,capacity=20000, power=10000):
-        container = []
-        residual = []
-        exflow = []
-        fillstand = capacity / 2
-        for w,d in zip(self.data["my_renew"],self.data["my_demand"]):
-            p = w-d
-            diff = max(-power,min(power,p))
-            if p > 0:
-                fillstand = min(capacity, fillstand+diff)
-                if fillstand+diff > capacity:
-                    exflow.append(fillstand+diff-capacity)
-                else:
-                    exflow.append(0)                    
-                residual.append(0)
-            else:
-                if fillstand <= 0:
-                    fillstand = 0
-                    residual.append(fillstand+diff)
-                else:
-                    fillstand = fillstand+diff
-                    residual.append(0)
-                exflow.append(0)
-            container.append(fillstand)
-        self.data["battery"] = container
-        self.data["residual"] = residual
-        self.data["exflow"] = exflow
-        share = (sum(self.data["my_demand"]) + sum(residual))/sum(self.data["my_demand"])
-        results = pd.DataFrame([[capacity/1000,f"{(-sum(residual)/1000):.2f}",f"{(sum(exflow)/1000):.2f}", f"{share:.2f}"]], columns=["capacity MWh","residual MWh","exflow MWh", "share"])
-        self.battery_results = pd.concat([self.battery_results, results], ignore_index=True)
-        pass
-
-    def run_analysis(self):
-        """Run the analysis"""
-        if self.data is None:
-            print("❌ No data loaded!")
-            return
-        
-        logger.info("Starting analysis...")
-
-        self.print_results()
-        self.simulate_battery(capacity=5000, power=2500)
-        self.print_results_with_battery()
-        self.simulate_battery(capacity=10000, power=5000)
-        self.print_results_with_battery()
-        self.simulate_battery(capacity=20000, power=10000)
-        self.print_results_with_battery()
-        print(self.battery_results)
-        self.visualise()
-
-    def print_results(self):
-        print(f"reference region: {self.region}, demand: {(sum(self.data["total_demand"])/1000):.2f} GWh, solar: {(sum(self.data['solar'])/1000):.2f} GWh, wind {(sum(self.data["wind_onshore"])/1000):.2f} GWh")
-        print(f"total demand: {(sum(self.data["my_demand"])/1e6):.2f} GWh " +
-              f"total Renewable_Source: {(sum(self.data["my_renew"])/1e6):.2f} GWh")
-        print(f"total renewalbes: {(sum(self.pos)/1000):.2f} MWh, residual: {(sum(self.neg)/1000):.2f} MWh")
-        print(f"share without battery {(sum(self.pos)/self.my_total_demand):.2f}")
-
-    def print_results_with_battery(self):
-        res = -sum(self.data["residual"])
-        print(f"total renewalbes: {(sum(self.pos)/1000):.2f} MWh, residual: {(res/1000):.2f} MWh, export: {(sum(self.data["exflow"])/1000):.2f} MWh")
-        print(f"share with battery: {((self.my_total_demand - res)/self.my_total_demand):.2f}")
-        pass
-
-    def visualise(self):        
-        fig, [ax1, ax2, ax3, ax4] = plt.subplots(4, 1, sharex=True)
-        ax1.plot(self.data.index, self.data["my_renew"], color="green")
-        ax1.plot(self.data.index, self.data["my_demand"], color="red")
-        ax1.set_ylabel("[kWh]")
-        ax1.set_title(f"Renewable_Source and Demand ({self.region})")
-        ax1.legend(["Renewable_Source", "Demand"])
-        ax1.grid(True)
-        ax2.plot(self.data.index, np.maximum(0,self.data["my_renew"]-self.data["my_demand"]), color="green")
-        ax2.plot(self.data.index, np.minimum(0,self.data["my_renew"]-self.data["my_demand"]), color="red")
-        ax2.legend(["Renewable_Source-Demand", "Residual"])
-        ax2.set_title("Renewable_Source-Demand")
-        ax2.set_xlabel("Date")
-        ax2.set_ylabel("[kWh]")
-        ax2.grid(True)
-        ax3.plot(self.data.index, self.data["battery"], color = "blue")
-        # ax3.legend(["battery"])
-        ax3.set_title("battery fillstand")
-        ax3.set_ylabel("[kWh]")
-        ax4.plot(self.data.index, self.data["residual"], color="red")
-        #ax4.legend(["-Demand"])
-        ax4.set_title("Residual")
-        ax4.set_xlabel("Date")
-        ax4.set_ylabel("[kWh]")
-        ax4.grid(True)
-        plt.show()
-
-        pass
-
 
 def main(argv = []):
     """Main function"""
